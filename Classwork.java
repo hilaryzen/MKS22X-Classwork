bda83d6da52d76f4ca85bee91c23c5bb484f97f1
Hilary Zen <hzen00@stuy.edu>
2019-02-05 22:06:23 -0500

    Added maxLetter

--- Classwork.java
+++ Classwork.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
 
 public class Classwork {
-  public static ArrayList<String> makeAllWords(int k) {
+  public static ArrayList<String> makeAllWords(int k, int maxLetter) {
     ArrayList<String> data = new ArrayList<String>();
-    wordsH(k, "", data);
+    wordsH(k, "", data, maxLetter);
     return data;
   }
 
-  public static void wordsH(int k, String word, ArrayList<String> data) {
+  public static void wordsH(int k, String word, ArrayList<String> data, int maxLetter) {
     if (k == 0) {
       data.add(word);
     } else {
-      for (int i = 0; i < 3; i++) {
+      for (int i = 0; i < maxLetter; i++) {
         int newL = 'a' + i;
         char newLetter = (char)newL;
-        wordsH(k - 1, word + newLetter, data);
+        wordsH(k - 1, word + newLetter, data, maxLetter);
       }
       //wordsH(k - 1, word + "a", data);
       //wordsH(k - 1, word + "b", data);
@@ -23,7 +23,7 @@ public class Classwork {
   }
 
   public static void main(String[] args) {
-    System.out.println(makeAllWords(1));
-    System.out.println(makeAllWords(2));
+    System.out.println(makeAllWords(1, 26));
+    System.out.println(makeAllWords(2, 3));
   }
 }
