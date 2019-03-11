2099d5d6290ee0f051a6f02e49eb734e5943cd7d
Hilary Zen <hzen00@stuy.edu>
2019-03-10 21:59:23 -0400

    Started partition

--- Classwork.java
+++ Classwork.java
@@ -22,10 +22,29 @@ public class Classwork {
     }
   }
 
+  /*Modify the array such that:
+  *1. Only the indices from start to end inclusive are considered in range
+  *2. A random index from start to end inclusive is chosen, the corresponding
+  *   element is designated the pivot element.
+  *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+  *@return the index of the final position of the pivot element.
+  */
+  public static int partition(int[] data, int start, int end) {
+    int pivotIndex = (int)(Math.random() * (end - start)) + start;
+    System.out.println(pivotIndex);
+    return 0;
+  }
+
   public static void main(String[] args) {
+    /*
     System.out.println(makeAllWords(3, 3));
     System.out.println(makeAllWords(4, 3));
     System.out.println(makeAllWords(1, 26));
     System.out.println(makeAllWords(2, 10));
+    */
+
+    int[] data = {5, 9, 10, 2, 4, 6, 3};
+    System.out.println(partition(data, 4, 6));
   }
 }
